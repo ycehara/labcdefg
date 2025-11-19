bc32f1f4db40646cc4abe62d5164be8b97fa70a6
ycehara <mtchct@gmail.com>
2025-11-19 14:59:01 +0800

    Adding all other works from LAB A

---
+++ labcdefg/labcdefg/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int fave = 18;
+    float height = 154.94;
+    char  initial = 'M';
+
+    printf("My favorite number is %d, fave");
+    printf("My height is %f, height");
+    printf("The initial of my first name is %c, initial");
+    printf("sample blabla")
+     printf("devv branch changes saample statement")
+    printf("devv202 branch changes sample statement")
+    return 0;
+}
