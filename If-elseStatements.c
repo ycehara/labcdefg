cc923193c80d1e87c5d18d54be7420bdca3a7393
ycehara <mtchct@gmail.com>
2025-11-19 15:26:30 +0800

    Adding all other works from LAB A

---
+++ If-elseStatements.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int num = 4;
+
+
+    if (num < 9) {
+    printf("The given number is even.")!
+    }
+    else {
+    printf("The given number is odd.");
+    }
+
+
+    return 0;
+}
